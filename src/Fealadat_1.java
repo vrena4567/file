df52189011ac4e2390df84412d4ef198aa4e087d
vrena4567 <v.rena4567@gmail.com>
2023-06-29 19:21:16 +0200

    rdy

--- src/Fealadat_1.java
+++ src/Fealadat_1.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Fealadat_1 {
     public static void main(String[] args) {
-
+        File file = new File("peldaStrings.txt");
+        List<String> lines = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("A file nem található");
+        }
+        System.out.println(lines);
     }
 }
 
--- src/Feladat_2.java
+++ src/Feladat_2.java
@@ -1,6 +1,31 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Feladat_2 {
     public static void main(String[] args) {
-
+        File file = new File("szamok.txt");
+        List<String> lines = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("A file nem található");
+        }
+        int sum = 0;
+        for (var actual : lines) {
+            for ( var actualNumber : actual.split(" ")) {   // a split metódus a szóközök mentén szétválasztja a stringeket
+                sum += Integer.parseInt(actualNumber);
+            }
+        }
+        System.out.println(lines);
+      System.out.println(sum);
     }
 }
 
